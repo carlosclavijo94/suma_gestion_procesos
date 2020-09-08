af1cbbe9819d41a3c83bd20a957a078495d4bd97
carlosclavijo94 <cmclavijo@espe.edu.ec>
2020-09-08 11:36:11 -0500

    suma de dos numeros primos

--- SumaGestionProcesos/src/gestionprocesos/Suma.java
+++ SumaGestionProcesos/src/gestionprocesos/Suma.java
@@ -15,15 +15,52 @@ public class Suma {
 		num1=Float.parseFloat(n1);
 		num2=Float.parseFloat(n2);
 		
-		float res;
-		res= num1+num2;
-		JOptionPane.showMessageDialog(null, "Respuesta: " + res);
+		int i,j;
+		int a=0;
+		int b=0;
+		int ver=0;
+		int ver1=0;
+		
+		for(i=1;i<(num1+1);i++){
+	         if(num1%i==0){
+	             a++;
+	            }
+	         }
+	         if(a!=2){
+	              System.out.println("No es Primo");
+	            }else{
+	            	ver=1;
+	                System.out.println("Si es Primo");
+	         }
+	         
+	         
+	 		for(j=1;j<(num2+1);j++){
+		         if(num2%j==0){
+		             b++;
+		            }
+		         }
+		         if(b!=2){
+		              System.out.println("No es Primo");
+		            }else{
+		            	ver1=1;
+		                System.out.println("Si es Primo");
+		         }
 		
 
 		
+		
+		float res;
+		
+		if(ver==1 & ver1==1) {
+			res= num1+num2;
+			JOptionPane.showMessageDialog(null, "Respuesta: " + res);
+		}else {
+			JOptionPane.showMessageDialog(null, "Por favor ingrese dos numeros primos");
+		}
+
+		
+		
 	}
-	
-	
 	
 	
 	
